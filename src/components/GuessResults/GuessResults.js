732e173da0b285b26172a91ac2ead0c8467c05b9
Stephanie Giang <stephaniegiang@users.noreply.github.com>
2023-10-21 15:51:12 +0000

    update to just use index

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,8 +3,8 @@ import React from 'react';
 function GuessResults({ guessList }) {
   return (
     <div className="guess-results">
-      {guessList.map((guess) => (
-        <p key={crypto.randomUUID()} className="guess">{guess}</p>
+      {guessList.map((guess, index) => (
+        <p key={index} className="guess">{guess}</p>
       ))}
     </div>
   );
